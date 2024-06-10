62ffdbe5fe0d82948ef5dcee8253667a84d66a49
kiranabbili <101236471+kiranabbili@users.noreply.github.com>
2024-06-10 21:53:50 +0530

    Update line_navigation.py

--- line_navigation.py
+++ line_navigation.py
@@ -1,13 +1,18 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
+LINEAR_SPEED=0.2
+KP=1.5/100
+
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
+        self.publisher=self.create_publisher(Twist,'/cmd_vel',10)
         self.subscription = self.create_subscription(
             Image,
             '/camera/image_raw',
@@ -33,29 +38,39 @@ class ImageSubscriber(Node):
         # Apply the mask to the original image
         blue_segmented_image = cv2.bitwise_and(current_frame, current_frame, mask=blue_mask)
 
+        line=self.get_contour_data(blue_mask)
+
+        cmd=Twist()
+        _,width,_=blue_segmented_image.shape
+        if line:
+            x=line['x']
+            error=x-width//2
+            cmd.linear.x=LINEAR_SPEED
+            cv2.circle(blue_segmented_image,(line['x'],line['y'],5,(0,0,255),7))
+
+        cmd.angular.z=float(error)* -KP
+        print("error: {} | Angular z: {}," .format(error,cmd.angular.z))
+        self.publisher.publish(cmd)
+        
         # Display the segmented image
         cv2.imshow("Blue Segmented Image", blue_segmented_image)
         cv2.waitKey(1)
 
-def get_contour_data(self,mask):
+    def get_contour_data(self,mask):
 
-    MIN_AREA_TRACK=50
+        MIN_AREA_TRACK=1100
 
-    contours,_ =cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        contours,_ =cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-    line={}
+        line={}
 
-    for contour in contours:
-        M=cv2.moments(contour)
+        for contour in contours:
+            M=cv2.moments(contour)
 
-        if (M['m00']>MIN_AREA_TRACK):
-            
-
-
-        line['x']
-
-
-    
+            if (M['m00']>MIN_AREA_TRACK):
+                line["x"]=int(M["m10"]/M["m00"])
+                line["y"]=int(M["m01"]/M["m00"])
+        return (line)
 
 def main(args=None):
     rclpy.init(args=args)
